071da8e3672deb1306b8ea092de4900c128c1641
naveenniverthy <naveenniverthy@gmail.com>
2026-04-24 22:03:43 -0400

    some imporvement

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/guided-path",
+        destination: "/path",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
